959880f6875ee8922e3d68b88ad115ab3f0baae0
SumalathaDesireddy <sumalathadesireddy@gmail.com>
2024-11-16 20:48:00 +0530

    hello change

--- UpwardIQ/UpwardIQ/src/main/java/com/suma/UpwardIQ/UpwardIqApplication.java
+++ UpwardIQ/UpwardIQ/src/main/java/com/suma/UpwardIQ/UpwardIqApplication.java
@@ -9,7 +9,7 @@ public class UpwardIqApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UpwardIqApplication.class, args);
 		
-		System.out.println("Hello Customer");
+		System.out.println("Hello customer!!");
 	}
 
 }
