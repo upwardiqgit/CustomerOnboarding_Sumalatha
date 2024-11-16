35fd69f7d83ac20e0f56b92837e92ae0466b38e8
SumalathaDesireddy <sumalathadesireddy@gmail.com>
2024-11-16 20:37:33 +0530

    First

---
+++ UpwardIQ/UpwardIQ/src/main/java/com/suma/UpwardIQ/UpwardIqApplication.java
@@ -0,0 +1,15 @@
+package com.suma.UpwardIQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpwardIqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpwardIqApplication.class, args);
+		
+		System.out.println("Hello world");
+	}
+
+}
---
+++ UpwardIQ/UpwardIQ/src/test/java/com/suma/UpwardIQ/UpwardIqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suma.UpwardIQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpwardIqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
