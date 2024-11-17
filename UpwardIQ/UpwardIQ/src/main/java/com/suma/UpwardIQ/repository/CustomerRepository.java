d8184f6e2cda0d9ceae80a974d104c6bdc824b41
SumalathaDesireddy <sumalathadesireddy@gmail.com>
2024-11-17 14:10:32 +0530

    repo change

--- UpwardIQ/UpwardIQ/src/main/java/com/suma/UpwardIQ/repository/CustomerRepository.java
+++ UpwardIQ/UpwardIQ/src/main/java/com/suma/UpwardIQ/repository/CustomerRepository.java
@@ -1,5 +1,6 @@
 package com.suma.UpwardIQ.repository;
 
+
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,13 +12,12 @@ import com.suma.UpwardIQ.model.Customer;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+	
 	boolean existsByCustomerId(String customerId); 
 	
-	// Custom query to filter by customer name and gender
-    @Query("SELECT c FROM Customer c WHERE " +
-           "(:name IS NULL OR c.name LIKE %:name%) AND " +
-           "(:gender IS NULL OR c.gender = :gender)")
-    List<Customer> findByCustomerNameAndGender(
-            @Param("name") String customerName,
-            @Param("gender") String gender);
+	@Query("SELECT c FROM Customer c WHERE c.name = :name AND c.gender = :gender")
+	List<Customer> findByCustomerNameAndGender(
+	        @Param("name") String name,
+	        @Param("gender") String gender);
 }
+
